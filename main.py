087b42b09dfa81dece514f429c348607b6c4bd80
Suresh <nsk126@gmail.com>
2023-05-18 04:48:12 -0700

    rnn

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import argparse
 import numpy as np
+import torch
+from torch import nn
 
 from src.load_dataset import Load_data as loader
-# Function to perform one-hot encoding
+from src.models import RNN as myRNN
 
+# Function to perform one-hot encoding
 def one_hot_encoding(char):
     # Create an array of zeros of shape (95,)
-    one_hot = np.zeros((95,))
+    one_hot = torch.zeros((95,))
 
     # Find the ASCII value of the character, subtract 32 to get an index in range 0-94
     # and set the corresponding index in the array to 1
     one_hot[ord(char) - 32] = 1
 
-    return one_hot
+    return torch.tensor(one_hot, dtype=torch.float32)
 
 def main():
     _data = 'datasets/train-articles'
@@ -21,10 +24,56 @@ def main():
     my_dataset = data.get_labeled_data()
     # print(my_dataset['111111112']['data'])
 
-    for txt in my_dataset['111111112']['data']:
-        for char in txt:
-            print(one_hot_encoding(char))
-    # print(one_hot_encoding('A'))
+    labels = ['non-propaganda', 'propaganda']
+    label_to_onehot = {'non-propaganda': torch.tensor([[1, 0]], dtype=torch.float32), 'propaganda': torch.tensor([[0, 1]], dtype=torch.float32)}
+
+    rnn = myRNN(95, 128, 2)
+    criterion = nn.MSELoss()
+    lr = 0.005
+    optimizer = torch.optim.Adam(rnn.parameters(), lr=lr)
+
+    # print(label_to_onehot[my_dataset['111111112']['labels'][0]])
+    # breakpoint()
+
+    epochs = 20
+
+    for epoch in range(epochs):
+
+        running_loss = 0.0
+        i = 0
+        hidden = rnn.initHidden()
+
+        for txt in my_dataset['111111112']['data']:
+            optimizer.zero_grad()
+
+            line = txt.split('\n')
+            line = line[0] # fix
+            # torch.autograd.set_detect_anomaly(True)
+
+            for char in line:
+                nn_input = one_hot_encoding(char)
+                output, hidden = rnn(nn_input.unsqueeze(0), hidden)
+
+            # print(labels[torch.argmax(output).item()])
+
+            target = label_to_onehot[my_dataset['111111112']['labels'][i]]
+
+
+            # calc loss
+            loss = criterion(output, target)
+
+            # training steps
+            optimizer.step()
+            loss.backward(retain_graph=True)
+
+            running_loss += loss
+
+            # update label
+            i += 1
+
+            # print(f'op = {output}, tg = {target}')
+            # print(labels[torch.argmax(output).item()])
+        print(f'epoch {epoch} - running loss = {running_loss}')
 
 
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,11 +3,21 @@ from torch import nn
 
 
 class RNN(nn.Module):
-    def __init__(self):
-        super(RNN. self).__init__()
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
 
-    def forward(self):
-        pass
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+        self.hidden_size = hidden_size
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
 
     def initHidden(self):
-        pass
+        return torch.zeros(1, self.hidden_size, dtype=torch.float32)
